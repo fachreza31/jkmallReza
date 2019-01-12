2749fb0ac6eaa9c3a8ba513b5c89a493effe64dc
fachreza audrian naufal <fachrezaaudriannaufal@gmail.com>
2019-01-12 15:48:35 +0700

    Initial Commit

---
+++ JakmallListView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JakmallListView
+//
+//  Created by Fachreza Audrian Naufal on 1/12/19.
+//  Copyright © 2019 Fachreza Audrian Naufal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
